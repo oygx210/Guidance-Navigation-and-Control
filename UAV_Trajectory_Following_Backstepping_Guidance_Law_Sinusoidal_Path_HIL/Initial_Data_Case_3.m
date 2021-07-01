74ead90148affb9b353765f13bcd573a077a7485
Hossein <hosseinzamani1995@gmail.com>
2021-07-01 20:46:03 +0430

    Plots, inital data, and simulation of UAV trajectory following using backstepping guidance law (Sinusoidal Path HIL (Arduino))

---
+++ UAV_Trajectory_Following_Backstepping_Guidance_Law_Sinusoidal_Path_HIL/Initial_Data_Case_3.m
@@ -0,0 +1,16 @@
+%% Initial_Data_Guidance_and_Navigation_Project_Case_3
+
+%% Target
+Vg_t = 10;
+gammadot_t = 0*pi/180;
+
+%% Commands Constant
+c1 = 10;
+c2 = 10;
+c3 = 10;
+alpha1 = 0.5;
+alpha2 = 0.5;
+alpha3 = 0.5;
+landa1 = 1;
+landa2 = 1;
+landa3 = 1;
