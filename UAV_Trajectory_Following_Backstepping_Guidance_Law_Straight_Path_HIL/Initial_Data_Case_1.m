09c04f058cae9e11a609ff7ab350f126a9e61112
Hossein <hosseinzamani1995@gmail.com>
2021-07-01 20:49:28 +0430

    Plots, initial data, and simualtion of UAV trajectory following using backstepping guidance law (Straight Path HIL (Arduino))

---
+++ UAV_Trajectory_Following_Backstepping_Guidance_Law_Straight_Path_HIL/Initial_Data_Case_1.m
@@ -0,0 +1,17 @@
+%% Initial_Data_Guidance_and_Navigation_Project_Case_1
+
+%% Target
+Vg_t = 10;
+gamma_t = 90*pi/180;
+zita_t = 0;
+
+%% Commands Constant
+c1 = 10;
+c2 = 10;
+c3 = 10;
+alpha1 = 0.5;
+alpha2 = 0.5;
+alpha3 = 0.5;
+landa1 = 1;
+landa2 = 1;
+landa3 = 1;
